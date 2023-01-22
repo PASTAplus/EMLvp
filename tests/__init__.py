4d8f3bd2b180461843a8687c4a5c1323aaed1415
Mark Servilla <mark.servilla@gmail.com>
2023-01-21 18:00:20 -0700

    Add test init

---
+++ tests/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: __init__
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    1/21/23
+"""
+import logging
+import os
+import sys
+
+import daiquiri
+
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+logfile = cwd + "/tests.log"
+daiquiri.setup(level=logging.DEBUG,
+               outputs=(daiquiri.output.File(logfile), "stdout",))
+logger = daiquiri.getLogger(__name__)
+
+
+sys.path.insert(0, os.path.abspath("../src"))
+test_data_path = os.path.abspath(os.path.dirname(__file__)) + "/data"

a1ef940ac648429977975be8770a6abde4b8cf5e
Mark Servilla <mark.servilla@gmail.com>
2023-01-23 16:00:09 -0700

    Refactor to use single-pass DOM parsing

--- src/emlvp/parser.py
+++ src/emlvp/parser.py
@@ -45,10 +45,23 @@ def _references(e: etree.Element) -> list:
     return references
 
 
+def _custom_units(e: etree.Element) -> list:
+    custom_units = []
+    refs = e.findall(".//{*}customUnit")
+    for ref in refs:
+        custom_units.append(ref.text.strip())
+
+    return custom_units
+
+
 class Parser(object):
 
     def __init__(self, fail_fast: bool = True):
         self.fail_fast = fail_fast
+        self.ids = []
+        self.references = []
+        self.custom_units = []
+        self.circular_references = []
 
     def parse(self, xml: str):
         # Accept either file or a string for source of EML XML
@@ -63,11 +76,10 @@ class Parser(object):
 
         doc = etree.fromstring(xml)
 
-        ids = _ids(doc)
-        references = _references(doc)
+        self._parse(doc)
 
         # Varify id uniqueness
-        id_duplicates = set([x for x in ids if ids.count(x) > 1])
+        id_duplicates = set([x for x in self.ids if self.ids.count(x) > 1])
         duplicate_id = False
         if len(id_duplicates) > 0:
             duplicate_id = True
@@ -78,14 +90,61 @@ class Parser(object):
 
         # Varify reference ids exist
         missing_reference_id = False
-        for r in references:
+        for r in self.references:
             references_without_ids = []
-            if r not in ids:
+            if r not in self.ids:
                 references_without_ids.append(r)
                 missing_reference_id = True
         if missing_reference_id:
-            msg_reference = f"Reference(s) missing Id(s): {references}"
+            msg_reference = f"Reference(s) missing Id(s): {self.references}"
             logger.error(msg_reference)
             if self.fail_fast:
                 raise exceptions.MissingReferenceIdError(msg_reference)
 
+        # Varify circular reference does not exist
+        if len(self.circular_references) > 0:
+            msg_circular = f"Circular id/reference(s) exists: {self.circular_references}"
+            logger.error(msg_circular)
+            if self.fail_fast:
+                raise exceptions.CircularReferenceIdError(msg_circular)
+
+        # Varify custom unit definition(s)
+        missing_custom_unit_definition = False
+        missing_custom_units = []
+        for cu in self.custom_units:
+            if cu not in self.ids:
+                missing_custom_units.append(cu)
+                missing_custom_unit_definition = True
+        if missing_custom_unit_definition:
+            msg_custom_unit = f"Custom unit(s) not defined: {missing_custom_units}"
+            logger.error(msg_custom_unit)
+            if self.fail_fast:
+                raise exceptions.CustomUnitError(msg_custom_unit)
+
+    def _parse(self, e: etree.Element):
+        """"Perform single-pass parsing of DOM analyzing each node"""
+
+        has_id = False
+        id = None
+        for attr in e.attrib:
+            if attr == "id":
+                has_id = True
+                id = e.attrib[attr]
+                self.ids.append(id)
+
+        has_reference = False
+        reference = None
+        for c in e.getchildren():
+            if c.tag == "references":
+                has_reference = True
+                reference = c.text.strip()
+                self.references.append(reference)
+
+        if has_id and has_reference:
+            self.circular_references.append(f"ID::{id}, REFERENCE::{reference}")
+
+        if e.tag == "customUnit":
+            self.custom_units.append(e.text.strip())
+
+        for c in e.getchildren():
+            self._parse(c)

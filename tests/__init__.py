4892ae2f7a49aa9088b14002e065e2e55c68009a
Mark Servilla <mark.servilla@gmail.com>
2023-02-01 14:49:04 -0700

    Refactor paths again

--- tests/__init__.py
+++ tests/__init__.py
@@ -20,10 +20,10 @@ import daiquiri
 
 
 cwd = Path("../tests").resolve().as_posix()
-logfile = cwd + "/tests.log"
-daiquiri.setup(level=logging.DEBUG,
-               outputs=(daiquiri.output.File(logfile), "stdout",))
-logger = daiquiri.getLogger(__name__)
+# logfile = cwd + "/tests.log"
+# daiquiri.setup(level=logging.DEBUG,
+#                outputs=(daiquiri.output.File(logfile), "stdout",))
+# logger = daiquiri.getLogger(__name__)
 
 sys.path.insert(0, Path("../src").resolve().as_posix())
 test_data_path = cwd + "/data"

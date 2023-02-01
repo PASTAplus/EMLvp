b35b1439685b8b36f62b53e6aac81ccc24a6ad41
Mark Servilla <mark.servilla@gmail.com>
2023-02-01 14:25:44 -0700

    Refactor cwd path

--- tests/__init__.py
+++ tests/__init__.py
@@ -19,15 +19,13 @@ import sys
 import daiquiri
 
 
-cwd = Path.cwd()
-logfile = cwd.as_posix() + "/tests.log"
+cwd = Path(".").resolve().as_posix()
+logfile = cwd + "/tests.log"
 daiquiri.setup(level=logging.DEBUG,
                outputs=(daiquiri.output.File(logfile), "stdout",))
 logger = daiquiri.getLogger(__name__)
 
-_ = Path("../src").resolve()
-
 sys.path.insert(0, Path("../src").resolve().as_posix())
-test_data_path = cwd.as_posix() + "/data"
+test_data_path = cwd + "/data"
 schema_path = Path("../src/emlvp/schemas").resolve().as_posix()
 
